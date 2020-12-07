dd8c1a3e12714f35929e98dead4e99503caf18ec
Caglar Toklu <caglartoklu@gmail.com>
2020-12-07 13:58:03 +0300

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import setuptools
 
 setuptools.setup(
     name="shotlast",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/caglartoklu/shotlast",
 
     author="Caglar Toklu",
